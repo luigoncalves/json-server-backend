818183c19cec8bc539a5fc80332914b0ab523150
Nijat Ismayilov <nijat.ismayilov@outlook.com>
2024-01-15 18:39:19 +0000

    username replaced by email | added functionality to authorize and return token when logged in

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-require("dotenv").config();
+// Required modules
 const jsonServer = require("json-server");
 const morgan = require("morgan");
+const jwt = require("jsonwebtoken");
 
+// Loading Environment Variables
+require("dotenv").config();
+
+// Json server instance
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT;
+
 
 server.use(middlewares);
 server.use(morgan("dev"));
@@ -14,8 +20,35 @@ server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
 });
+
+// Login route
+server.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  // Auth user
+  const user = router.db.get("users").find({ username, password }).value();
+
+  if (user) {
+    // Creating JWT token
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET);
+
+    // Updating user data with token
+    router.db
+      .get("users")
+      .find({ username, password })
+      .assign({ token })
+      .write()
+
+    res.json({ token })
+  } else {
+    res.status(401).json({ error: 'Invalid credentials' })
+  }
+})
+
+// Using Json server as router
 server.use(router);
 
+// Start the server
 server.listen(PORT, () => {
   console.log(`JSON Server is running at port ${PORT}`);
 });
