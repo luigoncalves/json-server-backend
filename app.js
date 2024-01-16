0612b4fd1496ed59716be172c92ad2c0ea46c3ce
Luis Gonçalves <goncalveshenrique34@gmail.com>
2024-01-16 20:17:59 +0000

    forgot smth

--- app.js
+++ app.js
@@ -14,3 +14,8 @@ server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
+server.use(router);
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running at port ${PORT}`);
+});
